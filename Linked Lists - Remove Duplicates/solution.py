d584eacbbc63768e8b9079dcb84e47b5b4054ef8
rini1ko <zaits.pn@ucu.edu.ua>
2026-03-19 21:41:32 +0200

    Linked Lists - Remove Duplicates solution

---
+++ Linked Lists - Remove Duplicates/solution.py
@@ -0,0 +1,13 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    probe=head
+    while probe and probe.next:
+        if probe.data==probe.next.data:
+            probe.next=probe.next.next
+        else:
+            probe=probe.next
+    return head
