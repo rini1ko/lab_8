3ce9a8ee0aeec1edf8d981b2cce00e5089f30274
rini1ko <zaits.pn@ucu.edu.ua>
2026-03-19 13:51:51 +0200

    Linked Lists - Move Node solution

---
+++ Linked Lists - Move Node/solution.py
@@ -0,0 +1,19 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    # Your code goes here.
+    # Remember to return the context.
+    if not source:
+        raise ValueError
+    el=source
+    source=source.next
+    el.next=dest
+    return Context(source, el)
